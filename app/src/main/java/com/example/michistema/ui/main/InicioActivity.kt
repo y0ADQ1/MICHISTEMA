1e3604224832702002891fde70a491af5b202c13
Brandon6183 <bryanrdzarr1@gmail.com.com>
2025-03-28 10:38:09 -0600

    s

--- app/src/main/java/com/example/michistema/ui/main/InicioActivity.kt
+++ app/src/main/java/com/example/michistema/ui/main/InicioActivity.kt
@@ -1,33 +1,46 @@
-package com.example.michistema.ui.components
+package com.example.michistema.ui.main
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.michistema.R
 import com.example.michistema.ui.auth.LoginActivity
-import com.example.michistema.ui.main.InicioActivity
+import com.example.michistema.ui.auth.RegisterActivity
 
-class SplashScreen : AppCompatActivity() {
+class InicioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash_screen)
+        setContentView(R.layout.activity_inicio)
+
+
+
+        val btnLogin: Button = findViewById(R.id.btnIniciarSesion)
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        val txtRegister: TextView = findViewById(R.id.linkRegistro)
+        txtRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, InicioActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 5000)
+
+
+
+
+        }
     }
 }
